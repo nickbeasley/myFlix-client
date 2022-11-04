5a1245157d169421ad4a2ba7155db7f77630c98e
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-11-04 10:51:19 -0500

    Update movie-view.jsx
    Added bootstrap and changed link to director view, genre view not yet working

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,36 +1,54 @@
-import React, { useState } from "react";
+import { React, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Button, Col, Row } from "react-bootstrap";
 
-import { useParams } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
 import "./movie-view.scss";
 import { MovieCard } from "../movie-card/movie-card";
 
 export function MovieView(props) {
-  // state = { movie: {} };
-  // keypressCallback(event) {
-  //   console.log(event.key);
-  // }
+  const { movieId } = useParams();
+  let navigate = useNavigate();
+
   let [movie, setMovie] = useState({});
-  //componentDidMount() {
-  document.addEventListener("keypress", this.keypressCallback);
-  const { id } = useParams();
-  console.log(id);
-  let film = props.movies.find((m) => m._id === id);
-  //setMovie(film);
-  //}
+  console.log("Movies from movie-view: ", movie);
 
-  // render() {
-  console.log("Movie: ", movie);
-  return <h1>Hello</h1>;
+  useEffect(() => {
+    let film = props.movies.find(function (m) {
+      return m._id == movieId;
+    });
+    setMovie(film);
+  }, []);
 
+  function addFavorite(movieId) {
+    const { movie } = props;
+    const username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+
+    const addFavorite = (e) => {
+      e.preventDefault();
+      axios
+        .post(
+          `https://nixflix.herokuapp.com/users/${username}/movies/${movie._id}`,
+          { username: localStorage.getItem("user") },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          alert("Movie added");
+        })
+        .catch((error) => console.error(error));
+    };
+  }
   return (
     <div className="movie-view">
       <Row>
         <Col>
           <div className="movie-poster">
-            <img src={movie.ImagePath} />
+            <img className="img-fluid" src={movie.ImagePath} />
           </div>
         </Col>
       </Row>
@@ -38,7 +56,6 @@ export function MovieView(props) {
       <Row>
         <Col>
           <div className="movie-title">
-            <span className="label">Title: </span>
             <span className="value">{movie.Title}</span>
           </div>
         </Col>
@@ -47,7 +64,6 @@ export function MovieView(props) {
       <Row>
         <Col>
           <div className="movie-description">
-            <span className="label">Description: </span>
             <span className="value">{movie.Description}</span>
           </div>
         </Col>
@@ -56,35 +72,65 @@ export function MovieView(props) {
       <Row>
         <Col>
           <div className="movie-genre">
-            <span className="label">Genre: </span>
-            <span className="value">{movie.Genre.Name}</span>
-            <Link to={`/genres/${movie.Genre.Name}`}>
-              <Button variant="link">Genre</Button>
+            <span className="label">Genre:</span>
+            <Link to={`/genres/${movie.Genre?.Name}`}>
+              <Button type="button" variant="link">
+                {movie.Genre?.Name}
+              </Button>
             </Link>
           </div>
         </Col>
       </Row>
-
+      {/* 
+link to DirectorView by sending it to the correct end point. 
+Line 95 is passing the value of "movie" (which is the current selected movie's  full array)
+from MovieView as "director" into DirectorView */}
       <Row>
         <Col>
           <div className="movie-director">
             <span className="label">Director: </span>
-            <span className="value">{movie.Director.Name}</span>
-            <Link to={`/directors/${movie.Director.Name}`}>
-              <Button variant="link">Director</Button>
-            </Link>
+            <Button
+              type="button"
+              variant="link"
+              onClick={() =>
+                navigate("/directors/:name", {
+                  state: { director: movie },
+                })
+              }
+            >
+              {movie.Director?.Name}
+            </Button>
           </div>
         </Col>
       </Row>
 
-      <Row>
+      <Col className="movie-view-favorite-button">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => addFavorite(movieId)}
+        >
+          Add to favorites
+        </button>
+      </Col>
+      {/* <Row>
         <Col>
           <div className="movie-featured">
             <span className="label">Featured: </span>
             <span className="value">{movie.featured ? "true" : "false"}</span>
           </div>
         </Col>
-      </Row>
+      </Row> */}
+
+      <Col className="movie-view-back-button">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          onClick={() => navigate("/", { replace: true })}
+        >
+          Go back
+        </button>
+      </Col>
     </div>
   );
 }
