9be15a32ac0d804d37a5a6fc1ef56ccc6d8802ef
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-11-04 10:48:33 -0500

    Update director-view.jsx
    Director view now works when clicking director name from MovieView

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,48 +1,50 @@
-import React from "react";
-import { Button, Container, Col, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+
+import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
+
+import { Button, Container, Col, Row, Button, Card } from "react-bootstrap";
+
 import { MovieCard } from "../movie-card/movie-card";
-import { PropTypes } from "react-bootstrap/esm/Image";
-export class DirectorView extends React.Component {
-  render() {
-    const { director, onBackClick, directorMovies } = this.props;
-    console.log(directors);
-    return (
-      <Container className="director-view">
+
+import PropTypes from "prop-types";
+
+export function DirectorView(props) {
+  const { name } = useParams();
+  let navigate = useNavigate();
+  // Here I'm telling state to useLocation which allows it to see the value being passed from MovieView
+  // and then telling director to equal the state (movie array) from MovieView
+  const { state } = useLocation();
+  const { director } = state;
+
+  let [setDirector, movie, movies, directorList] = useState({});
+
+  return (
+    <Container className="director-view">
+      <Card>
         <Row>
           <Col className="value">
-            <h1>{director.Director.Name}</h1>
+            <Card.Header>
+              <h1>{director.Director.Name}</h1>
+            </Card.Header>
             <p className="value">Birth: {director.Director.Birth}</p>
 
-            {director.Director.Death > 0 && (
-              <p className="value">Death: {director.Director.Death}</p>
-            )}
+            <p className="value">Death: {director.Director.Death}</p>
           </Col>
         </Row>
+
         <Row>
           <Col className="value">{director.Director.Bio}</Col>
         </Row>
-
-        <Row>
-          <Col className="pt-3">
-            <h3 className="label">Other {director.Director.Name} films:</h3>{" "}
-            {directorMovies.map((movie) => (
-              <Col lg={4}>
-                <MovieCard key={movie._id} movie={movie}>
-                  {movie.Title}
-                </MovieCard>
-              </Col>
-            ))}
-          </Col>
-        </Row>
-        <Button
-          className="mt-4"
-          onClick={() => {
-            onBackClick();
-          }}
-        >
-          Back
-        </Button>
-      </Container>
-    );
-  }
+        <Card.Footer>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => navigate("/", { replace: true })}
+          >
+            go back
+          </button>
+        </Card.Footer>
+      </Card>
+    </Container>
+  );
 }
