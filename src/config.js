964baebb4c218f1356e15f40182bf32b956249df
nickbeasley <109631653+nickbeasley@users.noreply.github.com>
2022-12-09 16:57:11 -0600

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const MOVIE_API_URL = "https://nixflix.netlify.app/.netlify/functions/server/";
+const MOVIE_API_URL = "https://nixflix.netlify.app/.netlify/functions/server";
 
 export { MOVIE_API_URL };
